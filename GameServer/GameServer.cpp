2ebd4dcbe327461f31ffe7212ab3e199b2ac1f85
JuYoung Kim <robolab1902@naver.com>
2023-01-05 14:22:08 +0900

    Multi Thread 예제 실험

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,8 +1,35 @@
 #include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+
+int32 sum = 0;
+
+void Add()
+{
+    for (int32 i = 0; i < 1000000; i++)
+    {
+        sum++;
+    }
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 1000000; i++)
+	{
+		sum--;
+	}
+}
 
 int main()
 {
-    HelloWorld();
+	Add();
+	Sub();
+	std::cout << sum << std::endl;
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+	std::cout << sum << std::endl;
 }
