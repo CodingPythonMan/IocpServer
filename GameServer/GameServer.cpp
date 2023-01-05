7a284737cc76c485eda53608d758e9794ebb54bf
JuYoung Kim <robolab1902@naver.com>
2023-01-05 18:45:00 +0900

    캐시 실습

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,82 +4,36 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <windows.h>
 #include <future>
 
-int64 result;
+int32 buffer[10000][10000];
 
-int64 Calculate()
-{
-	int64 sum = 0;
-
-	for (int32 i = 0; i < 1000000; i++)
-	{
-		sum += i;
-	}
-
-	return sum;
-}
-
-void PromiseWorker(std::promise<std::string>&& promise)
-{
-	promise.set_value("Secret_message");
-}
-
-void TaskWorker(std::packaged_task<int64(void)>&& task)
-{
-	task();
-}
 int main()
 {
-	// 동기(synchronous) 실행
-	//int64 sum = Calculate();
-	//std::cout << sum << std::endl;
+	memset(buffer, 0, sizeof(buffer));
 
-	// std::future
 	{
-		// 1) deferred => lazy evaluation 지연해서 실행하세요.
-		// 2) async => 별도의 스레드를 만들어서 실행하세요.
-		// 3) defereed | async => 둘 중 알아서 골라주세요
-		// 언젠가 미래에 결과물을 뱉어줄거야!
-		std::future<int64> future = std::async(std::launch::async, Calculate);
-	
-		// TODO
+		uint64 start = GetTickCount64();
 
-		int64 sum = future.get(); // 결과물이 이제서야 필요하다!
-	
-		//Knight knight;
-		//std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[i][j];
+
+		uint64 end = GetTickCount64();
+		std::cout << "Elapsed Tick " << (end - start) << std::endl;
 	}
 
-	// std::promise
 	{
-		// 미래(std::future) 에 결과물을 반환해줄거라 약속 해줘-
-		std::promise<std::string> promise;
-		std::future<std::string> future = promise.get_future();
+		uint64 start = GetTickCount64();
 
-		std::thread t(PromiseWorker, std::move(promise));
+		int64 sum = 0;
+		for (int32 i = 0; i < 10000; i++)
+			for (int32 j = 0; j < 10000; j++)
+				sum += buffer[j][i];
 
-		std::string message = future.get();
-		std::cout << message << std::endl;
-
-		t.join();
+		uint64 end = GetTickCount64();
+		std::cout << "Elapsed Tick " << (end - start) << std::endl;
 	}
-
-	// std::packaged_task
-	{
-		std::packaged_task<int64(void)> task(Calculate);
-		std::future<int64> future = task.get_future();
-
-		std::thread t(TaskWorker, std::move(task));
-
-		int64 sum = future.get();
-		std::cout << sum << std::endl;
-
-		t.join();
-	}
-
-	// 결론)
-	// mutex, condition_variable 까지 가지 않고 단순한 애들을 처리할 수 있는...
-	// 특히나, 한 번 발생하는 이벤트에 유용하다!
-	// 닭잡는데 소잡는 칼을 쓸 필요 없다!
 }
