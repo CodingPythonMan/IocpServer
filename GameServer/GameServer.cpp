31ba6821536cde9a7cf2cea15c8dff9a37bb1f4e
JuYoung Kim <robolab1902@naver.com>
2023-01-05 17:45:57 +0900

    Conditional Variable 을 소개하면서 조건부로 스레드를 대기 상태로 만들 수 있는 우아한 코드라고 칭찬하였다. 앞으로 이 방식을 쓰게 된다

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,18 +10,25 @@ std::mutex m;
 std::queue<int32> q;
 HANDLE handle;
 
+// 참고) cv 는 User-Level Object (커널 오브젝트x)
+std::condition_variable cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1. Lock을 잡고
+		// 2. 공유 변수 값을 수정
+		// 3. Lock을 풀고
+		// 4. 조건변수 통해 다른 스레드에게 통지
 		{
 			std::unique_lock<std::mutex> lock(m);
 			q.push(100);
 		}
 
-		::SetEvent(handle);
+		cv.notify_one(); // wait 중인 쓰레드가 있으면 딱 1개를 깨운다.
 
-		std::this_thread::sleep_for(std::chrono::microseconds(10000));
+		//std::this_thread::sleep_for(std::chrono::microseconds(10000));
 	}
 }
 
@@ -29,34 +36,31 @@ void Consumer()
 {
 	while (true)
 	{
-		::WaitForSingleObject(handle, INFINITE);
-		//::ResetEvent(handle);
-		// Non-Signal
-
 		std::unique_lock<std::mutex> lock(m);
-		if (q.empty() == false)
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1. lock 을 잡고
+		// 2. 조건 확인
+		// - 만족0 => 빠져나와서 이어서 코드를 진행
+		// - 만족x => Lock 을 풀어주고 대기 상태
+
+		// 그런데 notify_one 을 했으면 항상 조건식을 만족하는 거 아닐까?
+		// Spurious Makeup (가짜 기상?)
+		// notify_one 할 때 lock 을 잡고 있는 것이 아니기 때문
+
+		//if (q.empty() == false)
 		{
 			int32 data = q.front();
 			q.pop();
-			std::cout << data << std::endl;
+			std::cout << q.size() << std::endl;
 		}
 	}
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal (파란불) / Non-Signal (빨간불) << bool
-	// Auto / Manual
-	handle = ::CreateEvent(NULL/*보안 속성*/, FALSE/*bManualReset*/, 
-		FALSE/*bInitialState*/,NULL);
-
 	std::thread t1(Producer);
 	std::thread t2(Consumer);
 
 	t1.join();
 	t2.join();
-
-	::CloseHandle(handle);
 }
