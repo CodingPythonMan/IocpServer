0dad5f23373db02576167d0e397d0fcd061f04f1
JuYoung Kim <robolab1902@naver.com>
2023-01-10 11:56:00 +0900

    Stomp Allocator 는 메모리 오염을 잡는데 굉장히 효과적이다. 가상메모리와 페이징에 대해 정확하게 알아보았고 4kb 를 활용해 메모리 오버플로우나 delete 요소에 접근하지 못하게 크래시를 내주었다

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -11,7 +11,14 @@
 #include "RefCounting.h"
 #include "Memory.h"
 
-class Knight
+class Player 
+{
+public:
+	Player() {}
+	virtual ~Player() {}
+};
+
+class Knight : public Player
 {
 public:
 	Knight()
@@ -28,54 +35,19 @@ public:
 	{
 		cout << "~Knight()" << endl;
 	}
-	/*
-	static void* operator new(size_t size)
-	{
-		cout << "knight new! " << size << endl;
-		void* ptr = ::malloc(size);
-		return ptr;
-	}
-
-	static void operator delete(void* ptr)
-	{
-		cout << "knight delete! " << endl;
-		::free(ptr);
-	}*/
 
 	int32 _hp = 100;
 	int32 _mp = 10;
 };
 
-// new operator overloading (Global)
-void* operator new(size_t size)
-{
-	cout << "new! " << size << endl;
-	void* ptr = ::malloc(size);
-	return ptr;
-}
-
-void operator delete(void* ptr)
-{
-	cout << "delete! " << endl;
-	::free(ptr);
-}
-
-void* operator new[](size_t size)
-{
-	cout << "new[]! " << size << endl;
-	void* ptr = ::malloc(size);
-	return ptr;
-}
-
-void operator delete[](void* ptr)
-{
-	cout << "delete[]! " << endl;
-	::free(ptr);
-}
-
 int main()
 {
-	Knight* knight = xnew<Knight>(100);
+	// [          []]
+	Knight* knight = (Knight*)xnew<Player>();
+
+	knight->_hp = 100;
 
 	xdelete(knight);
+
+	
 }
