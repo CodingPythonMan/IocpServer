181f03ee083b834876892875b172874e6fd7e56f
JuYoung Kim <robolab1902@naver.com>
2023-01-13 19:04:14 +0900

    Protobuf 추가 중

--- GameServer/pch.h
+++ GameServer/pch.h
@@ -3,9 +3,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #ifdef _DEBUG
-#pragma comment(lib, "Debug\\ServerCore.lib")
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
 #else
-#pragma comment(lib, "Release\\ServerCore.lib")
+#pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
 #endif
 
 #include "CorePch.h"
