96b062bfa6476375c2467fddc45a1c80dbc5940d
JuYoung Kim <robolab1902@naver.com>
2023-01-20 11:56:20 +0900

    패킷 자동화 준비, proto 파일을 꽤 추가했고, 자동화 부분을 확인하였다

--- GameServer/ServerPacketHandler.cpp
+++ GameServer/ServerPacketHandler.cpp
@@ -1,23 +1,22 @@
 #include "pch.h"
 #include "ServerPacketHandler.h"
-#include "BufferReader.h"
-#include "BufferWriter.h"
 
-void ServerPacketHandler::HandlePacket(BYTE* buffer, int32 len)
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+// ÄÁÅÙÃ÷ ÀÛ¾÷ÀÚ
+
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
 {
-	BufferReader br(buffer, len);
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
 
-	PacketHeader header;
-	br.Peek(&header);
 
-	switch (header.id)
-	{
-	default:
-		break;
-	}
+
+	return false;
 }
 
-SendBufferRef ServerPacketHandler::MakeSendBuffer(Protocol::S_TEST& pkt)
+bool Handle_S_TEST(PacketSessionRef& session, Protocol::S_TEST& pkt)
 {
-	return _MakeSendBuffer(pkt, S_TEST);
+
+
+	return false;
 }
