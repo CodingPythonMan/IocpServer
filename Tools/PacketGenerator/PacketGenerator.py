92ba4fa86a69b31e0aabf28d0537d32d18e9a67d
JuYoung Kim <robolab1902@naver.com>
2023-01-25 10:23:11 +0900

    PacketGenerator.exe 가 나오지 않아 작업하였다

--- Tools/PacketGenerator/PacketGenerator.py
+++ Tools/PacketGenerator/PacketGenerator.py
@@ -5,7 +5,7 @@ import ProtoParser
 def main():
     
     arg_parser = argparse.ArgumentParser(description = 'PacketGenerator')
-    arg_parser.add_argument('--path', type=str, default='C:/practice/TsundereElf/Common/protoc-21.12-win64/bin/Protocol.proto', help='proto path')
+    arg_parser.add_argument('--path', type=str, default='C:/practice/TsundereElf/Common/Protobuf21/bin/Protocol.proto', help='proto path')
     arg_parser.add_argument('--output', type=str, default='TestPacketHandler', help='output file')
     arg_parser.add_argument('--recv', type=str, default='C_', help='recv convention')
     arg_parser.add_argument('--send', type=str, default='S_', help='send convention')
@@ -13,13 +13,13 @@ def main():
 
     parser = ProtoParser.ProtoParser(1000, args.recv, args.send)
     parser.parse_proto(args.path)
-    file_loader = jinja2.FileSystemLoader("Templates")
+    file_loader = jinja2.FileSystemLoader('Templates')
     env = jinja2.Environment(loader=file_loader)
 
     template = env.get_template('PacketHandler.h')
     output = template.render(parser = parser, output=args.output)
 
-    f = open(args.output+".h", 'w+')
+    f = open(args.output+'.h', 'w+')
     f.write(output)
     f.close()
 
